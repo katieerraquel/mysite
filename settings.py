31890c9fbf283b23c11a31a09dd8317049586813
katieerraquel <144392066+katieerraquel@users.noreply.github.com>
2025-01-23 10:58:06 -0700

    JavaScriptEssentials.py

--- settings.py
+++ settings.py
@@ -1,145 +1,78 @@
-# Django settings for mysite project.
+// Step 1: Create 2 variables favBook and favMovie and print them
+// Store your favorite book and movie in each variable as a string.
+let favBook = "To Kill a Mockingbird";  // Favorite book
+let favMovie = "The Shawshank Redemption";  // Favorite movie
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+// Print both variables to the console
+console.log("Favorite Book:", favBook);
+console.log("Favorite Movie:", favMovie);
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
 
-MANAGERS = ADMINS
+// Step 2: Create a constant called pi and print its value
+// Store the first 5 digits of pi in the constant
+const pi = 3.14159;
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+// Print the value of pi to the console
+console.log("Value of Pi:", pi);
+
+
+// Step 3: Create an array called favFoods and modify it
+// Store 5 favorite foods in an array
+let favFoods = ["Pizza", "Burger", "Sushi", "Pasta", "Ice Cream"];
+
+// Use array methods to add 2 items to the array
+favFoods.push("Tacos", "Chocolate");
+
+// Remove 1 item from the array
+favFoods.splice(2, 1);  // Removing the 3rd item (Sushi)
+
+// Print the final array to the console
+console.log("Favorite Foods:", favFoods);
+
+
+// Step 4: Create a variable called grade and construct an if/else if/else statement
+// Assign a numeric grade to the variable
+let grade = 85;
+
+// Use if/else if/else to determine the letter grade
+if (grade >= 90) {
+    console.log("Letter Grade: A");
+} else if (grade >= 80) {
+    console.log("Letter Grade: B");
+} else if (grade >= 70) {
+    console.log("Letter Grade: C");
+} else if (grade >= 60) {
+    console.log("Letter Grade: D");
+} else {
+    console.log("Letter Grade: F");
+}
+
+
+// Step 5: Create a for loop to print all numbers from 0 to 20
+for (let i = 0; i <= 20; i++) {
+    console.log(i);  // Print each number from 0 to 20
+}
+
+
+// Step 6: Create a function called findMax to return the larger of two numbers
+function findMax(num1, num2) {
+    if (num1 > num2) {
+        return num1;  // Return num1 if it is greater than num2
+    } else {
+        return num2;  // Return num2 if it is greater or equal to num1
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
+// Test the findMax function
+let maxNumber = findMax(25, 17);
+console.log("Larger number between 25 and 17:", maxNumber);
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'y5kr)k$3c3(gx+kbz+^i)49a_^a5!cv9c58f4(0bm%i1$xe$+2'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
-
-ROOT_URLCONF = 'mysite.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-)
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
+// Step 7: Create a function called customGreeting
+function customGreeting(name, major) {
+    return `Hello ${name}! How do you like taking ${major}?`;  // Return personalized greeting
 }
+
+// Test the customGreeting function
+let greetingMessage = customGreeting("John", "Computer Science");
+console.log(greetingMessage);  // Print the personalized greeting
